165e5f8c2a8633492ff98822d0c9251f61b7d6d8
Miijeh <81366976+Miijeh@users.noreply.github.com>
2021-05-28 10:39:27 +0200

    Pruebas testing no peta

--- src/test/java/rchat/CrearGrupoTest.java
+++ src/test/java/rchat/CrearGrupoTest.java
@@ -1,5 +1,11 @@
 package rchat;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assume.assumeFalse;
+import static org.junit.Assume.assumeTrue;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -7,11 +13,22 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 public class CrearGrupoTest {
+	boolean verdad;
 	
-	
-	
+	@BeforeEach
+	void verdadero() {
+		verdad=true;
+	}
 	@Test
 	void crearGrupo(){
+		assertTrue(verdad);
+		System.out.println("Verdad");
+	}
+	
+	@Test
+	void falso() {
+		System.out.println("Falso");
+		assertFalse(verdad);
 		
 	}
 }
